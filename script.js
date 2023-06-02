99c7022fcfc00b7c3770cf4499f0444c08980035
Kauã Xavier <kauaxavier032@gmail.com>
2023-06-02 10:04:13 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let allWeek = document.getElementById('weekValues')
 
+
+
+
 window.addEventListener("DOMContentLoaded" , async () => {
+    
+    let higherValue = 0
     await fetch("./data.json").then((response) => response.json()).then((data) =>{
-        data.map((value) =>{
+        data.map((value, index) =>{
+            value.amount > higherValue ? higherValue = value.amount : higherValue = higherValue;
+
             allWeek.innerHTML +=`
-            <div class="days" id="${value.day}"">
+            <div class="days"">
                 <p class="valueDay">$${value.amount}</p>
-                <div style="height: ${180 * (value.amount/100)}px"  class="divAmount"></div>
+                <div id="${value.day}" style="height: ${200 * (value.amount/100)}px"  class="divAmount"></div>
                 <p>${value.day}</p>
             </div>`
 
         })
     })
+    
 })
 
